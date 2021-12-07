24fe34dbccf674d64e87d48aa1e11ec2366776d5
Jeff Ward <jeff.ward@datadoghq.com>
2021-12-07 15:41:07 -0500

    Add Android support for initialization

--- android/src/main/kotlin/com/example/datadog_sdk/DatadogSdkPlugin.kt
+++ android/src/main/kotlin/com/example/datadog_sdk/DatadogSdkPlugin.kt
@@ -1,12 +1,28 @@
 package com.example.datadog_sdk
 
 import androidx.annotation.NonNull
+import com.datadog.android.bridge.DdBridge
+import com.datadog.android.bridge.DdSdkConfiguration
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+
+fun decodeDdSdkConfiguration(encoded: HashMap<String, Any?>): DdSdkConfiguration {
+  @Suppress("UNCHECKED_CAST")
+  return DdSdkConfiguration(
+    clientToken = encoded["clientToken"] as String,
+    env = encoded["env"] as String,
+    applicationId = encoded["applicationId"] as String?,
+    nativeCrashReportEnabled = encoded["nativeCrashReportEnabled"] as Boolean,
+    sampleRate = encoded["sampleRate"] as Double?,
+    site = encoded["site"] as String?,
+    trackingConsent = encoded["trackingConsent"] as String?,
+    additionalConfig = encoded["additionalConfig"] as Map<String, Any?>?
+  )
+}
 
 /** DatadogSdkPlugin */
 class DatadogSdkPlugin: FlutterPlugin, MethodCallHandler {
@@ -15,13 +31,23 @@ class DatadogSdkPlugin: FlutterPlugin, MethodCallHandler {
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private lateinit var channel : MethodChannel
+  private lateinit var binding: FlutterPlugin.FlutterPluginBinding;
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "datadog_sdk")
+    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "datadog_sdk_flutter")
     channel.setMethodCallHandler(this)
+
+    binding = flutterPluginBinding
   }
 
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+    when (call.method) {
+      "DdSdk.initialize" -> {
+        val configuration = call.argument<HashMap<String, Any?>>("configuration")
+          ?.let { decodeDdSdkConfiguration(it) }
+        DdBridge.getDdSdk(binding.applicationContext).initialize(configuration!!)
+      }
+    }
     if (call.method == "getPlatformVersion") {
       result.success("Android ${android.os.Build.VERSION.RELEASE}")
     } else {
