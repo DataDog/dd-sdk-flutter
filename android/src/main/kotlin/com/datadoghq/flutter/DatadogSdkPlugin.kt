f6770d1615b35aff60de7113c13a76b01be30081
Jeff Ward <jeff.ward@datadoghq.com>
2021-12-16 08:44:22 -0500

    Change Android package name from fluttersdk to flutter

--- android/src/main/kotlin/com/datadoghq/flutter/DatadogSdkPlugin.kt
+++ android/src/main/kotlin/com/datadoghq/flutter/DatadogSdkPlugin.kt
@@ -3,7 +3,7 @@
  * This product includes software developed at Datadog (https://www.datadoghq.com/).
  * Copyright 2016-Present Datadog, Inc.
  */
-package com.datadoghq.fluttersdk
+package com.datadoghq.flutter
 
 import androidx.annotation.NonNull
 import com.datadog.android.bridge.DdBridge
