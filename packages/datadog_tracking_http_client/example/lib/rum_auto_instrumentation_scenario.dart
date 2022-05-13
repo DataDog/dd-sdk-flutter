eef004de63c9b74324e5eee1edd7c82d7980ba90
Jeff Ward <jeff.ward@datadoghq.com>
2022-05-13 10:50:14 -0400

    ✅ Add manual traced call test case.
    Also fix CI by setting tracing to 100%

--- packages/datadog_tracking_http_client/example/lib/rum_auto_instrumentation_scenario.dart
+++ packages/datadog_tracking_http_client/example/lib/rum_auto_instrumentation_scenario.dart
@@ -2,7 +2,11 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2019-2022 Datadog, Inc.
 
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
 
 import 'custom_card.dart';
 import 'rum_auto_instrumentation_second_screen.dart';
@@ -45,22 +49,44 @@ class _RumAutoInstrumentationScenarioState
     }
   }
 
+  void _sendTraceableLog() async {
+    final clientToken = dotenv.get('DD_API_KEY', fallback: '');
+    final apiAppKey = dotenv.get('DD_APPLICATION_API_KEY', fallback: '');
+
+    var response = await http.get(
+      Uri.parse('https://api.datadoghq.com/api/v2/logs/events'),
+      headers: {
+        HttpHeaders.contentTypeHeader: 'application/json',
+        'DD-API-KEY': clientToken,
+        'DD-APPLICATION-KEY': apiAppKey,
+      },
+    );
+
+    // ignore: avoid_print
+    print('Got status response: ${response.statusCode}');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Auto RUM'),
       ),
-      body: ListView.builder(
-        itemCount: 2,
-        itemBuilder: (context, index) {
-          final item = images[index];
-          return CustomCard(
-            image: item,
-            text: 'Item $index',
-            onTap: () => _onTap(index),
-          );
-        },
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            for (int i = 0; i < images.length; ++i)
+              CustomCard(
+                image: images[i],
+                text: 'Item $i',
+                onTap: () => _onTap(i),
+              ),
+            ElevatedButton(
+              onPressed: _sendTraceableLog,
+              child: const Text('Send Traceable Log'),
+            ),
+          ],
+        ),
       ),
     );
   }
