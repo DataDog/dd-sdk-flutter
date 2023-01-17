646e6d3babc0f4578ab5eede1334bb62dc65dd92
Jeff Ward <jeff.ward@datadoghq.com>
2023-01-17 17:20:36 -0500

    👷 Created a script to make pinning versions easier

---
+++ tools/releaser/lib/package_list.dart
@@ -0,0 +1,18 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2022-Present Datadog, Inc.
+
+// Locations of Podfiles
+final podfileList = [
+  'packages/datadog_flutter_plugin/e2e_test_app/ios/Podfile',
+  'packages/datadog_flutter_plugin/example/ios/Podfile',
+  'packages/datadog_flutter_plugin/integration_test_app/ios/Podfile',
+  'packages/datadog_tracking_http_client/example/ios/Podfile',
+];
+
+// Location of gradle files
+final gradleList = [
+  'packages/datadog_flutter_plugin/android/build.gradle',
+  'packages/datadog_flutter_plugin/example/android/build.gradle',
+  'examples/native-hybrid-app/android/app/build.gradle',
+];
