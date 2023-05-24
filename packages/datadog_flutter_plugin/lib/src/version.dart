adf342bcc982b46418c9c1c9b10725e2b3ef5ecb
Jeff Ward <jeff.ward@datadoghq.com>
2023-05-24 13:20:11 +0200

    📝 Bump version of datadog_flutter_plugin to next potential release.

--- packages/datadog_flutter_plugin/lib/src/version.dart
+++ packages/datadog_flutter_plugin/lib/src/version.dart
@@ -2,4 +2,4 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-Present Datadog, Inc.
 
-const ddPackageVersion = '1.3.4';
+const ddPackageVersion = '1.3.5';
