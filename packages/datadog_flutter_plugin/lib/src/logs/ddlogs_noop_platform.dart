84b8d989b0943e13ac50e3af3c3877217fdc7f45
Jeff Ward <jeff.ward@datadoghq.com>
2023-07-07 14:27:33 -0400

    ✨ Add method to disable Datadog platform operations
    When performing widget tests in headless environments (without an emulator, simulator, or real device) There's not "platform" implementation for the DatadogSdk to work with, and it will throw a `MissingPluginException`.
    
    This change gives us a "NoOp" platform which we can use to essentially disable Datadog's functionality for headless tests.
    
    refs: RUMM-3370 #446

---
+++ packages/datadog_flutter_plugin/lib/src/logs/ddlogs_noop_platform.dart
@@ -0,0 +1,50 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2023-Present Datadog, Inc.
+
+import '../datadog_configuration.dart';
+import 'ddlogs_platform_interface.dart';
+
+class DdNoOpLogsPlatform extends DdLogsPlatform {
+  @override
+  Future<void> addAttribute(String loggerHandle, String key, Object value) {
+    return Future.value();
+  }
+
+  @override
+  Future<void> addTag(String loggerHandle, String tag, [String? value]) {
+    return Future.value();
+  }
+
+  @override
+  Future<void> createLogger(String loggerHandle, LoggingConfiguration config) {
+    return Future.value();
+  }
+
+  @override
+  Future<void> log(
+      String loggerHandle,
+      LogLevel level,
+      String message,
+      String? errorMessage,
+      String? errorKind,
+      StackTrace? errorStackTrace,
+      Map<String, Object?> attributes) {
+    return Future.value();
+  }
+
+  @override
+  Future<void> removeAttribute(String loggerHandle, String key) {
+    return Future.value();
+  }
+
+  @override
+  Future<void> removeTag(String loggerHandle, String tag) {
+    return Future.value();
+  }
+
+  @override
+  Future<void> removeTagWithKey(String loggerHandle, String key) {
+    return Future.value();
+  }
+}
