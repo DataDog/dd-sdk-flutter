be86573bba730c7d4c8f8324d390df2114e89315
Arthur Monteiro <monteiroamelo@gmail.com>
2024-05-28 16:04:19 -0300

    chore: remove unused import

--- packages/datadog_tracking_http_client/test/datadog_tracking_http_client_test.dart
+++ packages/datadog_tracking_http_client/test/datadog_tracking_http_client_test.dart
@@ -9,7 +9,6 @@ import 'package:datadog_common_test/uri_matchers.dart';
 import 'package:datadog_flutter_plugin/datadog_flutter_plugin.dart';
 import 'package:datadog_flutter_plugin/datadog_internal.dart';
 import 'package:datadog_tracking_http_client/datadog_tracking_http_client.dart';
-import 'package:datadog_tracking_http_client/src/tracking_http_client.dart';
 import 'package:datadog_tracking_http_client/src/tracking_http_client_plugin.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
