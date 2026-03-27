785b5f30d03218e879cd07b903cf26c40933a484
Jeff Ward <jeff.ward@datadoghq.com>
2026-03-27 15:26:08 -0400

    Fix Android test forge

--- packages/datadog_session_replay/android/src/test/kotlin/com/datadoghq/flutter/sessionreplay/forge/DatadogContextForgeryFactory.kt
+++ packages/datadog_session_replay/android/src/test/kotlin/com/datadoghq/flutter/sessionreplay/forge/DatadogContextForgeryFactory.kt
@@ -69,7 +69,10 @@ class DatadogContextForgeryFactory : ForgeryFactory<DatadogContext> {
                     locales = forge.aList { forge.aString() },
                     currentLocale = forge.aString(),
                     timeZone = forge.aString()
-                )
+                ),
+                logicalCpuCount = forge.anInt(),
+                totalRam = forge.aNullable { anInt() },
+                isLowRam = forge.aNullable { aBool() }
             ),
             userInfo = UserInfo(
                 id = forge.aNullable { anHexadecimalString() },
