e757a0d6892082688274c651576f9c4ecbec5d95
Jeff Ward <jeff.ward@datadoghq.com>
2026-09-21 12:09:13 -0400

    Fix duplicate symbol issue.

--- packages/datadog_session_replay/ios/datadog_session_replay/Package.swift
+++ packages/datadog_session_replay/ios/datadog_session_replay/Package.swift
@@ -13,13 +13,15 @@ let package = Package(
         .library(name: "datadog-session-replay", targets: ["datadog_session_replay", "datadog_session_replay_objc"])
     ],
     dependencies: [
-        .package(url: "https://github.com/Datadog/dd-sdk-ios.git", from: "3.0.0")
+        .package(url: "https://github.com/Datadog/dd-sdk-ios.git", from: "3.0.0"),
+        .package(name: "FlutterFramework", path: "../FlutterFramework")
     ],
     targets: [
         .target(
             name: "datadog_session_replay",
             dependencies: [
-                .product(name: "DatadogCore", package: "dd-sdk-ios")
+                .product(name: "DatadogCore", package: "dd-sdk-ios"),
+                .product(name: "FlutterFramework", package: "FlutterFramework")
             ],
             path: "Sources/Swift",
             resources: [],
